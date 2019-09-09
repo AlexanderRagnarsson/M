c6ee2cd55148857a0c451ebde21ac8aea3c2146f
AlexanderRagnarsson <alexanderrag19@ru.is>
2019-09-09 21:35:25 +0000

    Added comments to explain the code and a more descriptive variable

--- Verkefni/Verkefni 3/numbers.py
+++ Verkefni/Verkefni 3/numbers.py
@@ -1,10 +1,13 @@
-for i in range(10, 100):     
-    if (i//10 + i%10)**2 == i:
-            print(i)
-for i in range(1, 100):
+for number in range(10, 100):  
+    #Checks if the digit sum squared is equal to the number   
+    if (number//10 + number%10)**2 == number:
+            print(number)
+
+for number in range(1, 100):
     divisor_count = 0
-    for j in range(1, i+1):
-        if i%j == 0:
+    #Checks how many divisors number has
+    for possible_divisor in range(1, number+1):
+        if number%possible_divisor == 0:
             divisor_count += 1
     if divisor_count == 10:
-        print(i)
+        print(number)
