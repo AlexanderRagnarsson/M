cf58adc24b39bcca25a712b8de216d1b6c81bc76
AlexanderRagnarsson <alexanderrag19@ru.is>
2019-09-09 21:37:39 +0000

    Fixed one comment and added a space

--- Verkefni/Verkefni 3/numbers.py
+++ Verkefni/Verkefni 3/numbers.py
@@ -5,9 +5,10 @@ for number in range(10, 100):
 
 for number in range(1, 100):
     divisor_count = 0
-    #Checks how many divisors number has
+    #Counts how many divisors number has
     for possible_divisor in range(1, number+1):
         if number%possible_divisor == 0:
             divisor_count += 1
+
     if divisor_count == 10:
         print(number)
